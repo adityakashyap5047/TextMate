ba0e66df06e4b38f392d212e9b074c7309905426
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-04-29 13:27:10 +0530

    adding the requirements.txt file

--- TextMate/settings.py
+++ TextMate/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dqj!!bkdkm@0(l25(0j%5#okobcdt)r2(9kxcy#cnna*fhjq@5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
